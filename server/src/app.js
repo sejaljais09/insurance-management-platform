5801f13ed573a8544a626851c1de6ec27d0b4b7b
sejal jaiswal <sejaljaiswal716@gmail.com>
2026-07-22 23:47:50 +0530

    initialize Express backend

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Insurance Management API is running ",
+  });
+});
+
+module.exports = app;
